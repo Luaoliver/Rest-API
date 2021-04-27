e6d4e1a8fd6e0ca9ce926017501b85cd79ffb94c
luaoliver <lulu-so@hotmail.com>
2021-04-27 09:33:26 -0300

    updating and adding courses to the user

--- index.js
+++ index.js
@@ -2,27 +2,42 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-/** array pois ainda nãi há banco de dados */
+/** array pois ainda não há banco de dados */
 const usuarios = [];
-const cursos = [];
+const cursos = [
+    {
+        "id": 1, 
+        "nome": "Arquitetura"
+    }, 
+    {
+        "id": 2, 
+        "nome": "Jornalismo"
+    }
+];
 
 /** para fazer inclusões */
 /** primeiro defina os dados, o método post busca dados do body */
 /** PRIMEIRO é feita a requisição */
 server.post('/usuarios', (req, res) => {
     /** SEGUNDO o processamento */
+    const id = usuarios.length + 1;
     const nome = req.body.nome;
     const idade = req.body.idade;
-    const id = usuarios.length + 1;
-    const curso = cursos.push('');
-
+    const cursosRequest = req.body.cursos;
+    
+    const finalCursos = [];
+    cursos.forEach((item) => {
+        if(cursosRequest.includes(item.id)) {
+            finalCursos.push(item.nome)
+        } // includes verifica se no array cursosRequest possui item.id
+    })
     /** push serve para colocar informações dentro do array */
     /** quando o nome da chave e do valor coincidem basta anunciar apenas uma vez, por exemplo chave "nome" igual a valor "nome" */
     usuarios.push({
         id,
         nome,
         idade,
-        curso
+        "cursos": finalCursos
     });
 
     /** TERCEIRO retorna uma resposta */
@@ -35,10 +50,10 @@ server.get('/usuarios', (req, res) =>{
     return res.status(200).json(usuarios);
 });
 
-/** para atualização de dados */
+/** para atualização de idade */
 server.put('/usuarios/:id', (req, res) => {
-    const id = req.params.id;
-    const idade = req.body.idade; /** requisição */
+    const id = req.params.id; /** requisição por parametro na url */
+    const idade = req.body.idade; /** requisição por body*/
 
     usuarios[id - 1].idade = idade; /** lógica */
 
@@ -79,22 +94,49 @@ server.delete('/usuarios/:id', (req, res) => {
 });
 
 /** listar os curso */
-server.get('usuarios/:id/curso', (req, res) => {
-    return res.status(200).json(cursos);
+server.get('/usuarios/:id/cursos', (req, res) => {
+    const id = req.params.id;
+    const usuario = usuarios[id - 1];
+
+    return res.status(200).json(usuario.cursos);
 });
 
 /** incluir curso */
-server.post('usuarios/:id', (req, res) => {});
+server.post('/usuarios/:id/cursos', (req, res) => {
+    const id = req.params.id; //requisição por parametro na url
+    const cursosRequest = req.body.cursos; //requisição por body
+
+    const usuario = usuarios[id - 1];
+    cursos.forEach((curso) => { //vai olhar tudo que há dentro de cursos
+        if(cursosRequest.includes(curso.id)) {
+            usuario.cursos.push(curso.nome); // curso.nome aparecerá apenas o nome do curso
+        }
+    })
+
+    return res.status(201).json(usuarios);
+});
 
 /** alterar curso */
-server.put('usuarios/:id', (req, res) => {});
+/** server.put('/usuarios/cursos/:id', (req, res) => {}); */
 
 /** deletar curso */
-server.delete('usuarios/:id', (req, res) => {});
+server.delete('/usuarios/:id/cursos/:cursoid', (req, res) => {
+    const id = req.params.id;
+    const cursoid = req.params.cursoid;
 
 
+    cursos.forEach((curso) => {
+        if(curso.id == cursoid){
+            usuarios[id-1].cursos = usuarios[id-1].cursos.filter(
+                (usuarioCurso) => {
+                return usuarioCurso != curso.name;
+            });   
+        }
+    })
 
+    return res.status(200).json(usuarios[id-1]);
 
+});
 
 server.listen(3000, () => {
     console.log('Server is running in http://localhost:3000/')
